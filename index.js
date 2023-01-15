97b8e02e3d9a5a21886367bb911833afba3961e5
JoshuaBentley <joshuabentley911@gmail.com>
2023-01-15 11:07:10 -0700

    creating a mark down format with answers

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const fs = require('fs');
 
 const questions = [
     {
-    type: 'input',
-    name: 'project',
-    message: 'What is the name of your project?'
-}
+        type: 'input',
+        name: 'project',
+        message: 'What is the name of your project?'
+    },
+    {
+        type: 'input',
+        name: 'linkdin',
+        message: 'What is your linkdin?'
+    }
+
 ]
 
 inquirer.prompt(questions)
-    .then( answers => 
-        console.log(`My project name is ${answers.project}`)
-)
+    .then( answers => {
+        createReadme(answers)
+    }    
+);
+
+const createReadme = data => {
+    const literal = `
+    # The name of my projerct is ${data.project}!
+    ## If you have questions about it please got to me linkdin ${data.linkdin}! 
+    ` 
+    console.log(literal)
+}
