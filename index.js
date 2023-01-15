3a05fec42c88ccffd0c2932b74b545eda25cef87
JoshuaBentley <joshuabentley911@gmail.com>
2023-01-15 10:52:43 -0700

    can ask questions and display answers

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const inquierer = require('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
 
+const questions = [
+    {
+    type: 'input',
+    name: 'project',
+    message: 'What is the name of your project?'
+}
+]
+
+inquirer.prompt(questions)
+    .then( answers => 
+        console.log(`My project name is ${answers.project}`)
+)
